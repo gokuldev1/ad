16bd0a5e5b2ce27729074b81c7143eccd2aac972
gokuldev1 <gokulmohan.dev@gmail.com>
2025-03-11 16:36:07 +0530

    Update urls.py

--- adsoftwares/app/urls.py
+++ adsoftwares/app/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path,include
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     
     path('',views.index,name='index'),
    path("send-email", views.send_email, name="send_email"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
