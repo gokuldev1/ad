9431bbb9346919bd86173aac1f2d5b3455a35630
gokuldev1 <gokulmohan.dev@gmail.com>
2025-03-14 12:16:55 +0530

    Update urls.py

--- adsoftwares/app/urls.py
+++ adsoftwares/app/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     path('',views.index,name='index'),
-   path("send-email", views.send_email, name="send_email"),
+   path("send_email", views.send_email, name="send_email"),
 ]
 
 if settings.DEBUG:
